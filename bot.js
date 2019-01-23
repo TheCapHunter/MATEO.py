b722333973ca5e26f2ee70b571fbb3a10b4172ec
TheCapHunter <46630258+TheCapHunter@users.noreply.github.com>
2019-01-23 22:03:21 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,42 @@
+const Discord = require("discord.js");
+const fs = require("fs");
 
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("No no no.");
+  if(!args[0] || args[0 == "help"]) return message.reply("Usage: !prefix <:>");
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+
+  prefixes[message.guild.id] = {
+    prefixes: args[0]
+  };
+
+  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+    if (err) console.log(err)
+  });
+
+  let sEmbed = new Discord.RichEmbed()
+  .setColor("#FF9900")
+  .setTitle("Prefix Set!")
+  .setDescription(`Set to ${args[0]}`);
+
+  message.channel.send(sEmbed);
+
+}
+
+module.exports.help = {
+  name: "prefix"
+
+  module.exports.run = async (bot, message, args) => {
+  //!say Hi!
+  //Hi
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No");
+  let botmessage = args.join(" ");
+  message.delete().catch();
+  message.channel.send(botmessage);
+}
+
+module.exports.help = {
+  name: "say"
+}
